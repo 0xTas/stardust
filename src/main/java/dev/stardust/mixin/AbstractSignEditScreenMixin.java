dc3199b2e4fbb7c8fb3d36ff59de7eac5cc53362
0xTas <admin@0xtas.dev>
2024-03-15 18:01:54 -0700

    SignatureSign - Added options to automatically apply dyes and glow squid ink to signs. Also renamed the Truncate Lines option to Bypass Length Limits, and made it work when typing on the sign edit screen.

--- src/main/java/dev/stardust/mixin/AbstractSignEditScreenMixin.java
+++ src/main/java/dev/stardust/mixin/AbstractSignEditScreenMixin.java
@@ -4,12 +4,16 @@ import java.util.List;
 import java.util.Arrays;
 import java.util.ArrayList;
 import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.jetbrains.annotations.Nullable;
 import dev.stardust.modules.SignatureSign;
 import org.spongepowered.asm.mixin.Shadow;
 import net.minecraft.block.entity.SignText;
 import net.minecraft.client.gui.screen.Screen;
 import org.spongepowered.asm.mixin.injection.At;
+import net.minecraft.block.entity.SignBlockEntity;
+import net.minecraft.client.util.SelectionManager;
 import org.spongepowered.asm.mixin.injection.Inject;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -23,8 +27,19 @@ import net.minecraft.client.gui.screen.ingame.AbstractSignEditScreen;
 public abstract class AbstractSignEditScreenMixin extends Screen {
 
     @Shadow
+    private int currentRow;
+    @Shadow
+    @Final
+    private String[] messages;
+    @Shadow
     public abstract void close();
-
+    @Shadow
+    @Final
+    private SignBlockEntity blockEntity;
+    @Shadow
+    private @Nullable SelectionManager selectionManager;
+    @Shadow
+    protected abstract void setCurrentRowMessage(String message);
 
     protected AbstractSignEditScreenMixin(Text title) { super(title); }
 
@@ -39,7 +54,7 @@ public abstract class AbstractSignEditScreenMixin extends Screen {
 
         if (!String.join(" ", lines).trim().isEmpty()) return;
 
-        SignText signature = signatureSign.getSignature();
+        SignText signature = signatureSign.getSignature(this.blockEntity);
         List<String> msgs = Arrays.stream(signature.getMessages(false)).map(Text::getString).toList();
         String[] messages = new String[msgs.size()];
         messages = msgs.toArray(messages);
@@ -52,6 +67,21 @@ public abstract class AbstractSignEditScreenMixin extends Screen {
 
         if (signatureSign.getAutoConfirm()) {
             this.close();
+        }
+    }
+
+    @Inject(method = "init", at = @At("TAIL"))
+    private void disableWidthChecks(CallbackInfo ci) {
+        if (this.client == null) return;
+        SignatureSign signatureSign = Modules.get().get(SignatureSign.class);
+
+        if ((signatureSign.isActive() && signatureSign.signFreedom.get())) {
+            // bypass client-side length limits for sign text by using a truthy predicate in the SelectionManager
+            this.selectionManager = new SelectionManager(
+                () -> this.messages[this.currentRow], this::setCurrentRowMessage,
+                SelectionManager.makeClipboardGetter(this.client), SelectionManager.makeClipboardSetter(this.client),
+                string -> true
+            );
         }
     }
 }
