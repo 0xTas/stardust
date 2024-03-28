353507adfccc1fd146de098e18c2f6280230ab0c
0xTas <admin@0xtas.dev>
2024-03-27 18:49:24 -0700

    Added LoreLocator - Slot highlighter for rare, unique, or anomalous items.

---
+++ src/main/java/dev/stardust/mixin/DrawContextMixin.java
@@ -0,0 +1,31 @@
+package dev.stardust.mixin;
+
+import net.minecraft.item.ItemStack;
+import dev.stardust.modules.LoreLocator;
+import org.spongepowered.asm.mixin.Mixin;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Shadow;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.font.TextRenderer;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import meteordevelopment.meteorclient.systems.modules.Modules;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+
+/**
+ * @author Tas [0xTas] <root@0xTas.dev>
+ **/
+@Mixin(DrawContext.class)
+public abstract class DrawContextMixin {
+    @Shadow
+    public abstract void fill(int x1, int y1, int x2, int y2, int color);
+
+    // See LoreLocator.java
+    @Inject(method = "drawItemInSlot(Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/item/ItemStack;IILjava/lang/String;)V", at = @At(value = "HEAD"))
+    private void highlightNamedItems(TextRenderer textRenderer, ItemStack stack, int x, int y, @Nullable String countOverride, CallbackInfo ci) {
+        LoreLocator ll = Modules.get().get(LoreLocator.class);
+        if (!ll.isActive() || !ll.shouldHighlightSlot(stack)) return;
+        this.fill(x, y, x + 16, y + 16, ll.color.get().getPacked());
+    }
+}
