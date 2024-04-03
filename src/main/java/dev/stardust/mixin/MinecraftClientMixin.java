33d7d36d36bf787cb9ab401aac263460b38f124c
0xTas <admin@0xtas.dev>
2024-04-03 05:23:33 -0700

    RocketMan - Added delta-time calcs to keyboard control. This decouples your pitch & yaw rates from your FPS value, and keeps them consistent instead.

--- src/main/java/dev/stardust/mixin/MinecraftClientMixin.java
+++ src/main/java/dev/stardust/mixin/MinecraftClientMixin.java
@@ -4,6 +4,7 @@ import dev.stardust.modules.RocketMan;
 import net.minecraft.sound.MusicSound;
 import dev.stardust.modules.MusicTweaks;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import net.minecraft.client.MinecraftClient;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -17,10 +18,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
  **/
 @Mixin(MinecraftClient.class)
 public class MinecraftClientMixin {
+    @Unique
+    private long lastFrameTime = System.nanoTime();
 
     // See RocketMan.java
     @Inject(method = "render", at = @At("HEAD"))
     private void mixinRender(CallbackInfo ci) {
+        long currentTime = System.nanoTime();
+        float deltaTime = (currentTime - lastFrameTime) / 10000000f;
+
         RocketMan rocketMan = Modules.get().get(RocketMan.class);
         if (!rocketMan.isActive() || !rocketMan.shouldTickRotation()) return;
 
@@ -32,15 +38,15 @@ public class MinecraftClientMixin {
         switch (mode) {
             case "W Key" -> {
                 if (mc.player.input.sneaking) {
-                    mc.player.changeLookDirection(0.0f, rocketMan.getPitchSpeed());
+                    mc.player.changeLookDirection(0.0f, rocketMan.getPitchSpeed() * deltaTime);
                 } else if (mc.player.input.jumping) {
-                    mc.player.changeLookDirection(0.0f, -rocketMan.getPitchSpeed());
+                    mc.player.changeLookDirection(0.0f, -rocketMan.getPitchSpeed() * deltaTime);
                 }
 
                 if (mc.player.input.pressingRight) {
-                    mc.player.changeLookDirection(rocketMan.getYawSpeed(), 0.0f);
+                    mc.player.changeLookDirection(rocketMan.getYawSpeed() * deltaTime, 0.0f);
                 } else if (mc.player.input.pressingLeft) {
-                    mc.player.changeLookDirection(-rocketMan.getYawSpeed(), 0.0f);
+                    mc.player.changeLookDirection(-rocketMan.getYawSpeed() * deltaTime, 0.0f);
                 }
             }
             case "Spacebar" -> {
@@ -48,22 +54,22 @@ public class MinecraftClientMixin {
 
                 if (inverted) {
                     if (mc.player.input.pressingForward || mc.player.input.sneaking) {
-                        mc.player.changeLookDirection(0.0f, rocketMan.getPitchSpeed());
+                        mc.player.changeLookDirection(0.0f, rocketMan.getPitchSpeed() * deltaTime);
                     } else if (mc.player.input.pressingBack) {
-                        mc.player.changeLookDirection(0.0f, -rocketMan.getPitchSpeed());
+                        mc.player.changeLookDirection(0.0f, -rocketMan.getPitchSpeed() * deltaTime);
                     }
                 } else {
                     if (mc.player.input.pressingBack || mc.player.input.sneaking) {
-                        mc.player.changeLookDirection(0.0f, rocketMan.getPitchSpeed());
+                        mc.player.changeLookDirection(0.0f, rocketMan.getPitchSpeed() * deltaTime);
                     } else if (mc.player.input.pressingForward) {
-                        mc.player.changeLookDirection(0.0f, -rocketMan.getPitchSpeed());
+                        mc.player.changeLookDirection(0.0f, -rocketMan.getPitchSpeed() * deltaTime);
                     }
                 }
 
                 if (mc.player.input.pressingRight) {
-                    mc.player.changeLookDirection(rocketMan.getYawSpeed(), 0.0f);
+                    mc.player.changeLookDirection(rocketMan.getYawSpeed() * deltaTime, 0.0f);
                 } else if (mc.player.input.pressingLeft) {
-                    mc.player.changeLookDirection(-rocketMan.getYawSpeed(), 0.0f);
+                    mc.player.changeLookDirection(-rocketMan.getYawSpeed() * deltaTime, 0.0f);
                 }
             }
             case "Auto Use" -> {
@@ -71,26 +77,26 @@ public class MinecraftClientMixin {
 
                 if (inverted) {
                     if (mc.player.input.pressingForward || mc.player.input.sneaking) {
-                        mc.player.changeLookDirection(0.0f, rocketMan.getPitchSpeed());
+                        mc.player.changeLookDirection(0.0f, rocketMan.getPitchSpeed() * deltaTime);
                     } else if (mc.player.input.pressingBack || mc.player.input.jumping) {
-                        mc.player.changeLookDirection(0.0f, -rocketMan.getPitchSpeed());
+                        mc.player.changeLookDirection(0.0f, -rocketMan.getPitchSpeed() * deltaTime);
                     }
                 } else {
                     if (mc.player.input.pressingBack || mc.player.input.sneaking) {
-                        mc.player.changeLookDirection(0.0f, rocketMan.getPitchSpeed());
+                        mc.player.changeLookDirection(0.0f, rocketMan.getPitchSpeed() * deltaTime);
                     } else if (mc.player.input.pressingForward || mc.player.input.jumping) {
-                        mc.player.changeLookDirection(0.0f, -rocketMan.getPitchSpeed());
+                        mc.player.changeLookDirection(0.0f, -rocketMan.getPitchSpeed() * deltaTime);
                     }
                 }
 
                 if (mc.player.input.pressingRight) {
-                    mc.player.changeLookDirection(rocketMan.getYawSpeed(), 0.0f);
+                    mc.player.changeLookDirection(rocketMan.getYawSpeed() * deltaTime, 0.0f);
                 } else if (mc.player.input.pressingLeft) {
-                    mc.player.changeLookDirection(-rocketMan.getYawSpeed(), 0.0f);
+                    mc.player.changeLookDirection(-rocketMan.getYawSpeed() * deltaTime, 0.0f);
                 }
             }
         }
-
+        lastFrameTime = currentTime;
     }
 
     // See MusicTweaks.java
