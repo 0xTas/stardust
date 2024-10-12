8f5eb0483b2fb921ae1ea49d56df425df560b55f
0xTas <admin@0xtas.dev>
2024-10-11 18:03:59 -0700

    Added some new Stardust category icons to pick from, including 2b2t player heads.

--- src/main/java/dev/stardust/util/StardustUtil.java
+++ src/main/java/dev/stardust/util/StardustUtil.java
@@ -1,7 +1,9 @@
 package dev.stardust.util;
 
 import java.io.File;
+import java.util.UUID;
 import java.time.Instant;
+import java.util.Optional;
 import dev.stardust.Stardust;
 import net.minecraft.util.Hand;
 import net.minecraft.text.Text;
@@ -10,14 +12,17 @@ import net.minecraft.item.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.ClickEvent;
 import net.minecraft.text.MutableText;
+import com.mojang.authlib.GameProfile;
 import net.minecraft.network.packet.Packet;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
+import com.mojang.authlib.properties.Property;
 import net.minecraft.network.packet.c2s.play.*;
 import io.netty.util.internal.ThreadLocalRandom;
 import meteordevelopment.meteorclient.utils.Utils;
 import net.minecraft.component.DataComponentTypes;
 import dev.stardust.mixin.ClientConnectionAccessor;
+import net.minecraft.component.type.ProfileComponent;
 import static meteordevelopment.meteorclient.MeteorClient.mc;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import net.minecraft.network.encryption.NetworkEncryptionUtils;
@@ -145,8 +150,11 @@ public class StardustUtil {
     };
     private static final ItemStack[] menuIcons = {
         Items.CAKE.getDefaultStack(),
+        Items.SPAWNER.getDefaultStack(),
         Items.BEDROCK.getDefaultStack(),
         Items.GOAT_HORN.getDefaultStack(),
+        Items.HONEYCOMB.getDefaultStack(),
+        Items.LODESTONE.getDefaultStack(),
         Items.DRAGON_EGG.getDefaultStack(),
         Items.FILLED_MAP.getDefaultStack(),
         Items.PINK_TULIP.getDefaultStack(),
@@ -156,10 +164,12 @@ public class StardustUtil {
         Items.PINK_PETALS.getDefaultStack(),
         Items.WARPED_SIGN.getDefaultStack(),
         Items.CHERRY_SIGN.getDefaultStack(),
+        Items.WIND_CHARGE.getDefaultStack(),
         Items.WRITTEN_BOOK.getDefaultStack(),
         Items.DAMAGED_ANVIL.getDefaultStack(),
         Items.CHERRY_SAPLING.getDefaultStack(),
         Items.JACK_O_LANTERN.getDefaultStack(),
+        Items.KNOWLEDGE_BOOK.getDefaultStack(),
         Items.FIREWORK_ROCKET.getDefaultStack(),
         Items.TOTEM_OF_UNDYING.getDefaultStack(),
         Items.LIME_SHULKER_BOX.getDefaultStack(),
@@ -167,16 +177,56 @@ public class StardustUtil {
         Items.FLOWERING_AZALEA.getDefaultStack(),
         Items.PINK_SHULKER_BOX.getDefaultStack(),
         Items.GILDED_BLACKSTONE.getDefaultStack(),
+        Items.OMINOUS_TRIAL_KEY.getDefaultStack(),
         Items.HEART_POTTERY_SHERD.getDefaultStack(),
         Items.LIGHT_BLUE_SHULKER_BOX.getDefaultStack(),
         Items.ENCHANTED_GOLDEN_APPLE.getDefaultStack(),
         Items.HEARTBREAK_POTTERY_SHERD.getDefaultStack(),
+        Items.EYE_ARMOR_TRIM_SMITHING_TEMPLATE.getDefaultStack(),
         discIcons[ThreadLocalRandom.current().nextInt(discIcons.length)],
         doorIcons[ThreadLocalRandom.current().nextInt(doorIcons.length)],
         getCustomIcons()[ThreadLocalRandom.current().nextInt(getCustomIcons().length)]
     };
 
     private static ItemStack[] getCustomIcons() {
+        // Encoded profile textures taken from illegal player head items on 2b2t.org (except for mine.)
+        final String tasHeadTexture = "ewogICJ0aW1lc3RhbXAiIDogMTcyODQwNzM3MDc3MiwKICAicHJvZmlsZUlkIiA6ICJjZTA5ODE3NzBkMjc0NmY1YTM3ODUxODg5NzcxYmEyNyIsCiAgInByb2ZpbGVOYW1lIiA6ICIweFRhcyIsCiAgInNpZ25hdHVyZVJlcXVpcmVkIiA6IHRydWUsCiAgInRleHR1cmVzIiA6IHsKICAgICJTS0lOIiA6IHsKICAgICAgInVybCIgOiAiaHR0cDovL3RleHR1cmVzLm1pbmVjcmFmdC5uZXQvdGV4dHVyZS8yZGNlNGNlNWVhOWJjNWI1OTI1MmJlNDk1YTA5ZTQ0ZWFmMzc5NmRmNDY5OTU2MTdmZGQ4ZjFmMTBkNjU0ZjQyIgogICAgfQogIH0KfQ==";
+        final String popbobHeadTexture = "eyJ0aW1lc3RhbXAiOjE0MTYwOTQxOTU4NTYsInByb2ZpbGVJZCI6IjBmNzVhODFkNzBlNTQzYzViODkyZjMzYzUyNDI4NGYyIiwicHJvZmlsZU5hbWUiOiJwb3Bib2IiLCJpc1B1YmxpYyI6dHJ1ZSwidGV4dHVyZXMiOnsiU0tJTiI6eyJ1cmwiOiJodHRwOi8vdGV4dHVyZXMubWluZWNyYWZ0Lm5ldC90ZXh0dXJlLzEyNTY4ODQ4NWI3MjUxMWFmOWY4NzVjZjQ4NjlmNjYxOTkwNWU2ZjJjNzc3NGIyMjYxNTJjYTY3ODIzODFlNiJ9fX0=";
+        final String pyrobyteHeadTexture = "eyJ0aW1lc3RhbXAiOjE0MTYwOTQxOTUxOTUsInByb2ZpbGVJZCI6IjY4YjFiYjExY2ZhMzRlMTZhMDFkYjZkZGRhMGExMDgzIiwicHJvZmlsZU5hbWUiOiJQeXJvYnl0ZSIsImlzUHVibGljIjp0cnVlLCJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMzhjZTMwODMxYjU1YTI0MTFjMGYzMTI2ZDVhNThlMzE2NDZkNGE4YjZmMzYxZjcyMzc5ZGY0ZTY5OTE0OTkifX19";
+        final String iTristanHeadTexture = "ewogICJ0aW1lc3RhbXAiIDogMTcyODUwMDk2NjEwMywKICAicHJvZmlsZUlkIiA6ICI4ZDNmYTEyMmFjNGI0YjM1OGI1MzM5Mjc5NGJkZDU2MSIsCiAgInByb2ZpbGVOYW1lIiA6ICJUaGVTZW5wYWlPZjJiMnQiLAogICJzaWduYXR1cmVSZXF1aXJlZCIgOiB0cnVlLAogICJ0ZXh0dXJlcyIgOiB7CiAgICAiU0tJTiIgOiB7CiAgICAgICJ1cmwiIDogImh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZTcxNTFkNzliMDQzZWY5N2FkMjhhMjc5NDVmODY3OGRmMmE3OGU2NGE1MmQxYzkzMDgwNTdhMjFmMDQyMDNlNCIKICAgIH0KICB9Cn0=";
+        final String hausemasterHeadTexture = "eyJ0aW1lc3RhbXAiOjE0MTYwOTQxOTU2NjIsInByb2ZpbGVJZCI6IjhmMmNlNDUzY2VmMjRiM2ViNjg2ZGMyMWI1MTlhMGExIiwicHJvZmlsZU5hbWUiOiJIYXVzZW1hc3RlciIsImlzUHVibGljIjp0cnVlLCJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNGJiY2IyZTE5OTdjN2NiMWJkZjU2MTNkMTMyZWVjNmQ2NzEzM2EyMTYyMWUwZmFlMTU3YTZhZDhmOWIyIn19fQ==";
+        final String jackTheRippaHeadTexture = "eyJ0aW1lc3RhbXAiOjE0MTYwOTQxOTUxOTMsInByb2ZpbGVJZCI6IjdmMTk3NjE4MzJjMjQ4NzY4NDFiY2VhMjliZDU4Y2FlIiwicHJvZmlsZU5hbWUiOiJKYWNrdGhlcmlwcGEiLCJpc1B1YmxpYyI6dHJ1ZSwidGV4dHVyZXMiOnsiU0tJTiI6eyJ1cmwiOiJodHRwOi8vdGV4dHVyZXMubWluZWNyYWZ0Lm5ldC90ZXh0dXJlLzExYjk0OWE2MWZhNGNjOGZmZjNkM2I0OTY4MmQyZjk2ZjQxMThmOTI4ZDg2MjIyMmVmNjU2ZTMyYTVmMTIifX19";
+        final String cytoToxicTCellHeadTexture = "eyJ0aW1lc3RhbXAiOjE0MDY0MTc0NTE1MDgsInByb2ZpbGVJZCI6ImE0YTVlYmM0OWY0ZTQ3OTVhMjUzN2I4YjA1M2ZiMTdmIiwicHJvZmlsZU5hbWUiOiJDeXRvdG94aWNUY2VsbCIsImlzUHVibGljIjp0cnVlLCJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNTlkMWU2YzRmNjFkZmNmZGE2NDE3MjJmNjU3NzJiMTI3YmI0NDFkMGViMjU4YTM2Y2MxOTEzYmU3NTkyNGIxIn19fQ==";
+
+        // Get textures for the current player's head item
+        Optional<Property> currentPlayerProfileProperties = mc.getGameProfile().getProperties().get("textures").stream().findFirst();
+
+        String currentPlayerHeadTexture;
+        if (currentPlayerProfileProperties.isPresent()) {
+            currentPlayerHeadTexture = currentPlayerProfileProperties.get().value();
+        } else {
+            currentPlayerHeadTexture = "ewogICJ0aW1lc3RhbXAiIDogMTcyODQ5NzQxNzUwNCwKICAicHJvZmlsZUlkIiA6ICJkMDUwMzNmYzM3N2Q0OGU1ODFiMGJhYTY0NDBmNTIyOCIsCiAgInByb2ZpbGVOYW1lIiA6ICJQYXVsc3RldmUwMDciLAogICJzaWduYXR1cmVSZXF1aXJlZCIgOiB0cnVlLAogICJ0ZXh0dXJlcyIgOiB7CiAgICAiU0tJTiIgOiB7CiAgICAgICJ1cmwiIDogImh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNTk1YmQzOWQ5M2ZiYjI4NGVhNGEzYmJiMTljNzRlNTUxOGQwODRiNmZiMGQ5YjE1ZWQ2YzU2NzdmMDhkY2FhYyIKICAgIH0KICB9Cn0=";
+        }
+
+        String[] playerHeadTextures = {
+            currentPlayerHeadTexture, tasHeadTexture, popbobHeadTexture, pyrobyteHeadTexture,
+            iTristanHeadTexture, hausemasterHeadTexture, jackTheRippaHeadTexture, cytoToxicTCellHeadTexture
+        };
+
+        ItemStack playerHead = new ItemStack(Items.PLAYER_HEAD);
+        GameProfile profile = new GameProfile(UUID.randomUUID(), "Stardust");
+        ProfileComponent profileComponent = new ProfileComponent(profile);
+
+        // Apply a player head texture to the ItemStack
+        profileComponent.properties().put(
+            "textures",
+            new Property(
+                "textures", // Select a random player head texture from the playerHeadTextures array.
+                playerHeadTextures[ThreadLocalRandom.current().nextInt(playerHeadTextures.length)],""
+            )
+        );
+        playerHead.set(DataComponentTypes.PROFILE, profileComponent);
+
         ItemStack enchantedPick = new ItemStack(
             ThreadLocalRandom.current().nextInt(2) == 0 ? Items.DIAMOND_PICKAXE : Items.NETHERITE_PICKAXE);
         enchantedPick.set(DataComponentTypes.ENCHANTMENT_GLINT_OVERRIDE, true);
@@ -213,6 +263,7 @@ public class StardustUtil {
         ripTridentFly.set(DataComponentTypes.ENCHANTMENT_GLINT_OVERRIDE, true);
 
         return new ItemStack[] {
+            playerHead,
             enchantedPick, sword32k, illegalBow, bindingPumpkin, cgiElytra, ripTridentFly,
             enchantedGlass[ThreadLocalRandom.current().nextInt(enchantedGlass.length)]
         };
