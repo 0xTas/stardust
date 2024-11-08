af9b40c7b36993be7d242211cac4cee0f281ebad
0xTas <admin@0xtas.dev>
2024-11-07 22:04:14 -0800

    Added AutoSmith - Automatically upgrade gear or trim armor sets in smithing tables

--- src/main/java/dev/stardust/mixin/WorldMixin.java
+++ src/main/java/dev/stardust/mixin/WorldMixin.java
@@ -1,6 +1,7 @@
 package dev.stardust.mixin;
 
 import net.minecraft.world.World;
+import dev.stardust.modules.AutoSmith;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.world.WorldAccess;
@@ -13,19 +14,23 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-
 /**
  * @author Tas [0xTas] <root@0xTas.dev>
  **/
 @Mixin(World.class)
 public abstract class WorldMixin implements WorldAccess, AutoCloseable {
-    // See StashBrander.java
+    // See StashBrander.java && AutoSmith.java
     @Inject(method = "playSoundAtBlockCenter", at = @At("HEAD"), cancellable = true)
     private void mixinPlaySoundAtBlockCenter(BlockPos pos, SoundEvent sound, SoundCategory category, float volume, float pitch, boolean useDistance, CallbackInfo ci) {
         Modules modules = Modules.get();
         if (modules == null) return;
+        AutoSmith smith = modules.get(AutoSmith.class);
         StashBrander brander = modules.get(StashBrander.class);
-        if (!brander.isActive() || !brander.shouldMute()) return;
-        if (sound == SoundEvents.BLOCK_ANVIL_USE || sound == SoundEvents.BLOCK_ANVIL_BREAK) ci.cancel();
+        if (brander.isActive() && brander.shouldMute()) {
+            if (sound == SoundEvents.BLOCK_ANVIL_USE || sound == SoundEvents.BLOCK_ANVIL_BREAK) ci.cancel();
+        }
+        if (smith.isActive() && smith.muteSmithy.get()) {
+            if (sound == SoundEvents.BLOCK_SMITHING_TABLE_USE) ci.cancel();
+        }
     }
 }
