acc4795fa82a74be4371367035ccf7fb2f1f720b
0xTas <admin@0xtas.dev>
2025-07-22 23:37:40 -0700

    Improved LogUtil prefix formatting.

--- src/main/java/dev/stardust/util/LogUtil.java
+++ src/main/java/dev/stardust/util/LogUtil.java
@@ -7,24 +7,24 @@ public class LogUtil {
         Stardust.LOG.info("{} {}", MsgUtil.getRawPrefix(), msg);
     }
     public static void info(String msg, String module) {
-        Stardust.LOG.info("{} {}", MsgUtil.getRawPrefix(module), msg);
+        Stardust.LOG.info("{}{} {}", MsgUtil.getRawPrefix(), MsgUtil.getRawPrefix(module), msg);
     }
     public static void warn(String msg) {
         Stardust.LOG.warn("{} {}", MsgUtil.getRawPrefix(), msg);
     }
     public static void warn(String msg, String module) {
-        Stardust.LOG.warn("{} {}", MsgUtil.getRawPrefix(module), msg);
+        Stardust.LOG.warn("{}{} {}", MsgUtil.getRawPrefix(), MsgUtil.getRawPrefix(module), msg);
     }
     public static void error(String msg) {
         Stardust.LOG.error("{} {}", MsgUtil.getRawPrefix(), msg);
     }
     public static void error(String msg, String module) {
-        Stardust.LOG.error("{} {}", MsgUtil.getRawPrefix(module), msg);
+        Stardust.LOG.error("{}{} {}", MsgUtil.getRawPrefix(), MsgUtil.getRawPrefix(module), msg);
     }
     public static void debug(String msg) {
         Stardust.LOG.debug("{} {}", MsgUtil.getRawPrefix(), msg);
     }
     public static void debug(String msg, String module) {
-        Stardust.LOG.debug("{} {}", MsgUtil.getRawPrefix(module), msg);
+        Stardust.LOG.debug("{}{} {}", MsgUtil.getRawPrefix(), MsgUtil.getRawPrefix(module), msg);
     }
 }
